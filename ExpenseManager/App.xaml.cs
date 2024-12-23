eddbb4a8dbb73c59c3ccc1d7fd26a933e2c8524f
swostikagarga <118736712+swostikagarga@users.noreply.github.com>
2024-12-23 08:35:53 +0545

    set up

---
+++ ExpenseManager/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ExpenseManager
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
