6eba8fa939946dd7ac9a163efeb2595ed317a6ae
swostikagarga <118736712+swostikagarga@users.noreply.github.com>
2025-01-05 11:20:08 +0545

    second milestone

--- ExpenseManager/obj/Debug/net8.0-ios/iossimulator-x64/ExpenseManager.AssemblyInfo.cs
+++ ExpenseManager/obj/Debug/net8.0-ios/iossimulator-x64/ExpenseManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenseManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+eddbb4a8dbb73c59c3ccc1d7fd26a933e2c8524f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenseManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpenseManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
