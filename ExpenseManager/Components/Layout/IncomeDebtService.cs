c3992a62f75726610c58d50c753ac1b8a00bf9f4
swostikagarga <118736712+swostikagarga@users.noreply.github.com>
2025-02-15 23:50:55 +0545

    Initial commit

---
+++ ExpenseManager/Components/Layout/IncomeDebtService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExpenseManager.Components.Layout;
+
+
+public class IncomeDebtService
+{
+    // The total income value
+    public decimal TotalIncome { get; private set; } = 0m;
+
+    // Method to decrease income (this happens when clearing debt)
+    public void DecreaseIncome(decimal amount)
+    {
+        TotalIncome -= amount;
+    }
+
+    // Method to add income (when income transaction is added)
+    public void IncreaseIncome(decimal amount)
+    {
+        TotalIncome += amount;
+    }
+}
